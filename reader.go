6bb4bc841152d7caf5eea18090d0f98222f7ed59
Deep Kapadia <deep.kapadia@nytimes.com>
2014-08-18 18:33:25 -0400

    working cassandra inserts for distribution

--- reader.go
+++ reader.go
@@ -1,40 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/bitly/go-nsq"
+	"github.com/gocql/gocql"
 	"github.com/nytlabs/gojsonexplode"
 )
 
-/*
-func loop(inChan chan *nsq.Message) {
-	i := 0
-	for msg := range inChan {
-		exploded, err := gojsonexplode.Explodejson(msg.Body, "||")
-		if err != nil {
-			fmt.Println(err)
-			msg.Finish()
-		}
-		i = i + 1
-		fmt.Println(i)
-		msg.Finish()
-	}
-}
-*/
-
 func loop(inChan chan *nsq.Message) {
 	count := 0
+	unmarshaled := make(map[string]interface{})
+	outChan := make(chan string, 1)
+	go insertMap(outChan)
 	tick := time.NewTicker(10 * time.Second)
 	for {
 		select {
 		case m := <-inChan:
-			gojsonexplode.Explodejson(m.Body, "||")
+			exploded, err := gojsonexplode.Explodejson(m.Body, "||")
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			err = json.Unmarshal(exploded, &unmarshaled)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			for k := range unmarshaled {
+				outChan <- k
+			}
+
 			count++
 			m.Finish()
 		case <-tick.C:
 			fmt.Println(count)
+		}
+	}
+}
+
+func insertMap(inChan chan string) {
+	cluster := gocql.NewCluster("localhost:49171")
+	cluster.Keyspace = "distribution"
+	cluster.Consistency = gocql.Quorum
+	session, err := cluster.CreateSession()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer session.Close()
+	for {
+		select {
+		case m := <-inChan:
+			err = session.Query("UPDATE et_totals set count=count+1 WHERE key=?", m).Exec()
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
@@ -46,7 +68,7 @@ func main() {
 	lookup := "localhost:4161"
 	conf := nsq.NewConfig()
 	conf.Set("maxInFlight", 1000)
-	reader, err = nsq.NewConsumer("page", "tick", conf)
+	reader, err = nsq.NewConsumer("page", "tick#ephemeral", conf)
 	if err != nil {
 		fmt.Println(err)
 	}
