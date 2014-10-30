fcc891c0264ea2e431afe22c3f708c7097b3780c
Ubuntu <ubuntu@ip-10-142-176-60.ec2.internal>
2014-10-30 00:56:57 +0000

    better naming for reader and db. Breaks API currently

--- reader.go
+++ reader.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"runtime"
@@ -14,35 +15,41 @@ import (
 
 var cluster *gocql.ClusterConfig
 var session *gocql.Session
+var stream *string
 
 func loop(inChan chan *nsq.Message) {
 	count := 0
 	unmarshaled := make(map[string]interface{})
 	outChan := make(chan string, 100)
 	countChan := make(chan string, 100)
-	//for i := 0; i < 50; i++ {
+	tsChan := make(chan struct {
+		k string
+		v string
+	}, 100)
 	go insertMap(outChan)
-	//}
-	//for i := 0; i < 10; i++ {
 	go insertTotal(countChan)
-	//}
-	tick := time.NewTicker(10 * time.Second)
+	go insertData(tsChan)
+	tick := time.NewTicker(30 * time.Second)
 	for {
 		select {
 		case m := <-inChan:
-			exploded, err := gojsonexplode.Explodejson(m.Body, "||")
+			exploded, err := gojsonexplode.Explodejson(m.Body, ".")
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			countChan <- "event_tracker"
+			countChan <- *stream
 			err = json.Unmarshal(exploded, &unmarshaled)
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			for k := range unmarshaled {
+			for k, v := range unmarshaled {
 				outChan <- k
+				tsChan <- struct {
+					k string
+					v string
+				}{k, v}
 			}
 
 			count++
@@ -53,11 +60,12 @@ func loop(inChan chan *nsq.Message) {
 	}
 }
 
-func insertdata(string) {
-}
+func insertData(inChan chan struct {
+	k string
+	v string
+}) {
 
-func insertMap(inChan chan string) {
-	tick := time.NewTicker(10 * time.Second)
+	tick := time.NewTicker(30 * time.Second)
 	var insertmap map[string]int
 	insertmap = make(map[string]int)
 	for {
@@ -67,7 +75,7 @@ func insertMap(inChan chan string) {
 		case <-tick.C:
 			//loop through map and insert data here
 			for k, v := range insertmap {
-				err := session.Query("UPDATE et_totals set count=count+? WHERE key=?", v, k).Exec()
+				err := session.Query("UPDATE dist_over_time set count=count+? and event_time=? WHERE attr_name=? AND attr_val=?", c, t, k).Exec()
 				if err != nil {
 					log.Println(k + " : Is the EOF here?")
 					log.Println(err)
@@ -81,7 +89,7 @@ func insertMap(inChan chan string) {
 }
 
 func insertTotal(inChan chan string) {
-	tick := time.NewTicker(10 * time.Second)
+	tick := time.NewTicker(30 * time.Second)
 	var insertmap map[string]int
 	insertmap = make(map[string]int)
 	for {
@@ -91,7 +99,7 @@ func insertTotal(inChan chan string) {
 		case <-tick.C:
 			//loop through map and insert data here
 			for k, v := range insertmap {
-				err := session.Query("UPDATE event_count set count=count+? WHERE type=?", v, k).Exec()
+				err := session.Query("UPDATE total_events set count=count+? WHERE type=?", v, k).Exec()
 				if err != nil {
 					log.Println(k + " : Is the EOF here?")
 					log.Println(err)
@@ -112,24 +120,37 @@ func main() {
 	var reader *nsq.Consumer
 	var err error
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	inChan := make(chan *nsq.Message, 100)
+
+	//parse command line flags
+	stream = flag.String("stream", "page", "one of the streams available in NSQ")
+	channel := flag.String("channel", "tick", "channel for the tick app")
+	ephemeral := flag.Bool("ephemeral", true, "is the channel ephemeral. True by default")
+
+	flag.Parse()
+
+	if *ephemeral {
+		*channel = *channel + "#ephemeral"
+	}
+
+	inChan := make(chan *nsq.Message, 1000)
 	lookup := "10.238.154.138:4150"
 	//lookup := "ec2-50-17-119-19.compute-1.amazonaws.com:4161"
 	conf := nsq.NewConfig()
-	conf.Set("maxInFlight", 1000)
+	//conf.Set("maxInFlight", 1000)
+	conf.MaxInFlight = 1000
 	cluster = gocql.NewCluster("10.152.146.16")
-	cluster.Keyspace = "distribution"
+	cluster.Keyspace = "tick"
 	cluster.Consistency = gocql.One
 	session, err = cluster.CreateSession()
 	if err != nil {
 		log.Println(err)
-		log.Println("why?")
+		log.Println("Error initiating session with database")
 	}
 	defer session.Close()
-	reader, err = nsq.NewConsumer("page", "tick#ephemeral", conf)
+	reader, err = nsq.NewConsumer(*stream, *channel, conf)
 	if err != nil {
 		log.Println(err)
-		log.Println("why")
+		log.Println("Error connecting to nsq")
 	}
 	reader.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
 		inChan <- m
@@ -139,7 +160,7 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
 	loop(inChan)
 	<-reader.StopChan
-	//session.Close()
 }
